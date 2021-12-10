ac4d8887d10e51ba4f5a8ce72bd1461de19affaf
Richard Webster <richardw@indicee.com>
2021-12-10 01:04:56 -0800

    Day10b

--- src/Day10.hs
+++ src/Day10.hs
@@ -7,9 +7,9 @@ import MyPrelude
 -- import qualified Data.Map as Map
 -- import qualified Data.Set as Set
 import qualified Data.Text as T
--- import qualified Prelude
+import qualified Prelude
 
-import Utils
+-- import Utils
 
 
 -- | Answer: 415953
@@ -18,7 +18,7 @@ day10a = do
     chunks :: [Chunk] <- day10_readInput "data/day10a_input.txt"
 
     putText "Chunks:"
-    traverse_ putText chunks
+    traverse_ print chunks
     putText ""
 
     print $ day10a_compute chunks
@@ -28,60 +28,131 @@ day10a_compute !chunks =
     sum $
     mapMaybe checkChunkSyntax chunks
 
-type Chunk = Text
+newtype Chunk = Chunk [ChunkChar]
+
+instance Show Chunk where
+    show (Chunk chars) = concatMap show chars
+
 type SyntaxErrorScore = Int
+type AutocompleteScore = Int
+
+data ChunkType =
+    Round
+  | Square
+  | Curly
+  | Angle
+    deriving (Eq, Show)
+
+illegalClosingCharScore :: ChunkType -> SyntaxErrorScore
+illegalClosingCharScore Round  = 3
+illegalClosingCharScore Square = 57
+illegalClosingCharScore Curly  = 1197
+illegalClosingCharScore Angle  = 25137
+
+data ChunkChar =
+    Open  ChunkType
+  | Close ChunkType
+
+instance Show ChunkChar where
+    show (Open Round)   = "("
+    show (Open Square)  = "["
+    show (Open Curly)   = "{"
+    show (Open Angle)   = "<"
+    show (Close Round)  = ")"
+    show (Close Square) = "]"
+    show (Close Curly)  = "}"
+    show (Close Angle)  = ">"
+
+toChunkChar :: Char -> ChunkChar
+toChunkChar '(' = Open Round
+toChunkChar '[' = Open Square
+toChunkChar '{' = Open Curly
+toChunkChar '<' = Open Angle
+toChunkChar ')' = Close Round
+toChunkChar ']' = Close Square
+toChunkChar '}' = Close Curly
+toChunkChar '>' = Close Angle
+toChunkChar c   = panic $ "toChunkChar: invalid chunk char: " <> show c
 
 -- | If the chunk is invalid, then return the score corresponding
 -- to the first invalid char.
 checkChunkSyntax :: Chunk -> Maybe SyntaxErrorScore
-checkChunkSyntax !chunk =
+checkChunkSyntax (Chunk chars) =
     let
-        checkChunk :: [Char] -> [Char] -> Maybe SyntaxErrorScore
+        checkChunk :: [ChunkType] -> [ChunkChar] -> Maybe SyntaxErrorScore
         checkChunk _openCharsStack [] = Nothing
-        checkChunk openCharsStack (c : c_rest) | isOpenChar c =
+        checkChunk openCharsStack (Open c : c_rest) =
             checkChunk (c : openCharsStack) c_rest
-        checkChunk (s : s_rest) (c : c_rest) | isCloseChar c =
-            if c == matchingCloseChar s then checkChunk s_rest c_rest
+        checkChunk (s : s_rest) (Close c : c_rest) =
+            if c == s then checkChunk s_rest c_rest
             else Just $ illegalClosingCharScore c
         checkChunk openCharsStack c_rest =
-            panic $ "checkChunkSyntax: openCharsStack = " <> T.pack openCharsStack <> ", chunk = " <> T.pack c_rest
+            panic $ "checkChunkSyntax: openCharsStack = " <> show openCharsStack <> ", chunk = " <> show c_rest
     in
-        checkChunk [] $ T.unpack chunk
+        checkChunk [] chars
 
-matchingCloseChar :: Char -> Char
-matchingCloseChar '(' = ')'
-matchingCloseChar '<' = '>'
-matchingCloseChar '{' = '}'
-matchingCloseChar '[' = ']'
-matchingCloseChar c   = panic $ "matchingCloseChar :: not a closing char: " <> show c
+-- | If the chunk is incomplete, then return the score to autocomplete it.
+-- Note that the chunk must not be corrupt.
+checkChunkCompleteness :: Chunk -> Maybe AutocompleteScore
+checkChunkCompleteness (Chunk chars) =
+    let
+        checkChunk :: [ChunkType] -> [ChunkChar] -> Maybe AutocompleteScore
+        checkChunk [] [] = Nothing
+        checkChunk openCharsStack [] =
+            Just $ autocompleteScore openCharsStack
+        checkChunk openCharsStack (Open c : c_rest) =
+            checkChunk (c : openCharsStack) c_rest
+        checkChunk (s : s_rest) (Close c : c_rest) =
+            if c == s then checkChunk s_rest c_rest
+            else panic "checkChunkCompleteness - corrupt chunk found"
+        checkChunk openCharsStack c_rest =
+            panic $ "checkChunkSyntax: openCharsStack = " <> show openCharsStack <> ", chunk = " <> show c_rest
+    in
+        checkChunk [] chars
 
-isOpenChar :: Char -> Bool
-isOpenChar !c = c `elem` ['(', '[', '{', '<']
+autocompleteScore :: [ChunkType] -> AutocompleteScore
+autocompleteScore chunkTypes =
+    foldl' (\curScore ct -> 5 * curScore + chunkTypeAutoCompleteScore ct) 0 chunkTypes
 
-isCloseChar :: Char -> Bool
-isCloseChar !c = c `elem` [')', ']', '}', '>']
-
-illegalClosingCharScore :: Char -> SyntaxErrorScore
-illegalClosingCharScore ')' = 3
-illegalClosingCharScore ']' = 57
-illegalClosingCharScore '}' = 1197
-illegalClosingCharScore '>' = 25137
-illegalClosingCharScore c   = panic $ "illegalClosingCharScore :: not a closing char: " <> show c
+chunkTypeAutoCompleteScore :: ChunkType -> AutocompleteScore
+chunkTypeAutoCompleteScore Round  = 1
+chunkTypeAutoCompleteScore Square = 2
+chunkTypeAutoCompleteScore Curly  = 3
+chunkTypeAutoCompleteScore Angle  = 4
 
 
 day10_readInput :: FilePath -> IO [Chunk]
 day10_readInput !fileName = do
     fileTxt <- readFile fileName
     let fileLines :: [Text] = lines fileTxt
-    pure fileLines
+    pure $ map day10a_parseLine fileLines
+
+day10a_parseLine :: Text -> Chunk
+day10a_parseLine !txt =
+    Chunk $
+    map toChunkChar $
+    T.unpack txt
 
 
--- | Answer: ???
+-- | Answer: 2292863731
 day10b :: IO ()
 day10b = do
     chunks :: [Chunk] <- day10_readInput "data/day10a_input.txt"
     print $ day10b_compute chunks
 
 day10b_compute :: [Chunk] -> Int
-day10b_compute _chunks =
-    panic "xxx"
+day10b_compute !chunks =
+    let
+        nonCorruptChunks :: [Chunk]
+        nonCorruptChunks =
+            filter (\c -> checkChunkSyntax c == Nothing) chunks
+
+        autocompleteScores :: [AutocompleteScore]
+        autocompleteScores =
+            mapMaybe checkChunkCompleteness nonCorruptChunks
+        
+        medianScore :: AutocompleteScore
+        medianScore =
+            (sort autocompleteScores) !! (length autocompleteScores `div` 2)
+    in
+        medianScore
