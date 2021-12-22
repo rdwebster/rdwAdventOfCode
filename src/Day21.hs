e636b109484e5e6373701e91d113dd181084f926
Richard Webster <richardw@indicee.com>
2021-12-22 00:09:01 -0800

    Day21b

--- src/Day21.hs
+++ src/Day21.hs
@@ -6,7 +6,7 @@ module Day21
 import MyPrelude
 -- import Data.List.NonEmpty ((<|))
 -- import qualified Data.List.NonEmpty as NonEmpty
--- import qualified Data.Map.Strict as Map
+import qualified Data.Map.Strict as Map
 -- import qualified Data.Set as Set
 -- import qualified Data.Text as T
 -- import Data.Vector.Unboxed (Vector)
@@ -45,6 +45,8 @@ day21a_compute !p1Start !p2Start =
 type Player    = Int
 type Pos       = Int
 type Score     = Int
+type Count     = Int
+type NumWins   = Int
 type DiceRoll  = Int
 type DiceRolls = [DiceRoll]
 
@@ -123,14 +125,113 @@ playTurn _rolls _gameState =
     panic "unexpected - no more rolls"
 
 
--- | Answer: ???
+data GameStateB =
+    GameStateB {
+        p1PositionB :: !Pos
+      , p2PositionB :: !Pos
+      , p1ScoreB    :: !Score
+      , p2ScoreB    :: !Score
+    }
+    deriving (Eq, Ord, Show)
+
+-- | Instead of tracking each universe separately, keep track of how many universes
+-- are in each distinct state (p1/p2 positions and p1/p2 scores).
+playTurnB :: Player -> Map GameStateB Count -> Map GameStateB Count
+playTurnB !nextPlayer !gameStates =
+    let
+        newRollCombinations :: [DiceRolls]
+        newRollCombinations = do
+            r1 <- [1..3]
+            r2 <- [1..3]
+            r3 <- [1..3]
+            pure [r1, r2, r3]
+
+        rollTotals :: [Int]
+        rollTotals = sum <$> newRollCombinations
+
+        -- Update each known game state for each of the 27 universes for this turn.
+        updateGameState :: GameStateB -> Count -> [(GameStateB, Count)]
+        updateGameState !gameState !stateCount =
+            map (\rt -> (updateGameStateWithRoll gameState rt, stateCount)) rollTotals
+
+        updateGameStateWithRoll :: GameStateB -> Int -> GameStateB
+        updateGameStateWithRoll gs@GameStateB{..} !rollTotal
+            | nextPlayer == 1 =
+                let
+                    newPosition = ((p1PositionB + rollTotal - 1) `mod` 10) + 1
+                in
+                    gs{ p1PositionB = newPosition
+                      , p1ScoreB    = p1ScoreB + newPosition
+                      }
+            | otherwise =
+                let
+                    newPosition = ((p2PositionB + rollTotal - 1) `mod` 10) + 1
+                in
+                    gs{ p2PositionB = newPosition
+                      , p2ScoreB    = p2ScoreB + newPosition
+                      }
+    in
+        Map.fromListWith (+) $
+        concatMap (\(gs, cnt) -> updateGameState gs cnt) $
+        Map.toList gameStates
+
+isWinningGameStateB :: GameStateB -> Bool
+isWinningGameStateB GameStateB{..} =
+    p1ScoreB >= 21
+    || p2ScoreB >= 21
+
+runGameB :: Pos -> Pos -> (NumWins, NumWins)
+runGameB !p1Start !p2Start =
+    let
+        gameStates0 :: Map GameStateB Count
+        gameStates0 = Map.singleton (GameStateB p1Start p2Start 0 0) 1
+
+        go :: Player -> NumWins -> NumWins -> Map GameStateB Count -> (NumWins, NumWins)
+        go !nextPlayer !numWins1 !numWins2 !activeGameStates
+            | Map.null activeGameStates = (numWins1, numWins2)
+            | otherwise =
+                let
+                    updatesGameStates :: Map GameStateB Count
+                    updatesGameStates = playTurnB nextPlayer activeGameStates
+
+                    -- Separate out any winning game states.
+                    winningGameStates   :: Map GameStateB Count
+                    newActiveGameStates :: Map GameStateB Count
+                    (winningGameStates, newActiveGameStates) =
+                        Map.partitionWithKey (\gs _ -> isWinningGameStateB gs) updatesGameStates
+
+                    p1WinStates :: Map GameStateB Count
+                    p2WinStates :: Map GameStateB Count
+                    (p1WinStates, p2WinStates) =
+                        Map.partitionWithKey (\GameStateB{..} _ -> p1ScoreB > p2ScoreB) winningGameStates
+
+                    newWins1 :: NumWins
+                    newWins1 = sum $ Map.elems p1WinStates
+
+                    newWins2 :: NumWins
+                    newWins2 = sum $ Map.elems p2WinStates
+                in
+                    go (if nextPlayer == 1 then 2 else 1)
+                       (numWins1 + newWins1)
+                       (numWins2 + newWins2)
+                       newActiveGameStates
+    in
+        go 1 0 0 gameStates0
+
+-- | Answer: 158631174219251
 day21b :: IO ()
 day21b = do
-    let p1Start = 4
-        p2Start = 8
+    -- let p1Start = 4         -- Test
+    --     p2Start = 8
+    let p1Start = 10        -- Actual
+        p2Start = 9
     print $ day21b_compute p1Start p2Start
 
 day21b_compute :: Pos -> Pos -> Int
-day21b_compute _p1Start _p2Start =
-    panic "xxx"
-
+day21b_compute !p1Start !p2Start =
+    let
+        (p1Wins, p2Wins) = runGameB p1Start p2Start
+    in
+        traceVal "p1Wins = " p1Wins `seq`
+        traceVal "p2Wins = " p2Wins `seq`
+        max p1Wins p2Wins
